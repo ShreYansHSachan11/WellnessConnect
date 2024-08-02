0588f59c9918096442c14529e2c3a46d072a261e
Aastha <asthaagrawal46@gmail.com>
2024-08-02 21:34:59 +0530

    edited navbar design

--- Frontend/src/components/navbar/Navbar.jsx
+++ Frontend/src/components/navbar/Navbar.jsx
@@ -1,10 +1,8 @@
 import React from "react";
 import logo from "../../assets/logo.png";
-import { IoIosAddCircleOutline } from "react-icons/io";
 import "./Navbar.css";
 
 const Navbar = () => {
-  const style = { color: "white", fontSize: "1.5em" };
 
   return (
     <div className="navbar_container_main">
@@ -12,9 +10,6 @@ const Navbar = () => {
         <a href="/">
           <img src={logo} className="logo-img" />
         </a>
-        <div>
-          <IoIosAddCircleOutline style={style} />
-        </div>
       </nav>
     </div>
   );
